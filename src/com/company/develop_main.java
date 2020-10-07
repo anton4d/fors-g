a77abcb45f3376f121450c6360b9c4d4362e1268
Anton <antonlukas25@gmail.com>
2020-10-07 17:51:29 +0200

    ting og sager

--- src/com/company/develop_main.java
+++ src/com/company/develop_main.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class develop_main {
     public static void main(String[] args) {
-     System.out.println("Virker det?");
+
+        System.out.println("Virker det?");
     }
 }
